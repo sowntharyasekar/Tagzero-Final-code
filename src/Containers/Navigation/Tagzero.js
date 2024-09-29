85b5f993c9e20943b989911311e3feae433891be
sowntharya <sowntharya@clayfin.com>
2024-09-29 21:57:51 +0530

    nav bar functionality

--- src/Containers/Navigation/Tagzero.js
+++ src/Containers/Navigation/Tagzero.js
@@ -1,4 +1,6 @@
 import TagzeroNav from "./TagzeroNav";
+import React, { useRef } from "react";
+
 import { Backgroundimage } from "../../constants/svglocations";
 import AboutUs from "../../StaticComponents/AboutUs";
 import Service from "../../StaticComponents/Services";
@@ -6,17 +8,28 @@ import HowWeWork from "../../StaticComponents/HowWeWork";
 import Casestudies from "../../StaticComponents/Casestudies";
 import Footer from "../../StaticComponents/Footer";
 const Tagzero=()=>{
+    const aboutUs = useRef(null);
+    const services = useRef(null);
+    const howWeWork = useRef(null);
+    const caseStudy = useRef(null);
+    const imagesec = useRef(null);
+
+const scrollToSection=(sectionRef)=>{
+    sectionRef.current.scrollIntoView({ behavior: "smooth" });
+}
     return(
         <>
         {/* // Navbar */}
-        <TagzeroNav/>
+      
+        <TagzeroNav scrollToSection={scrollToSection} aboutUs={aboutUs} services={services} howWeWork={howWeWork} caseStudy={caseStudy} imagesec={imagesec}/>
+       
         {/* // Backgroundimage */}
         
       
             <Backgroundimage className="responsive-image">
                
             </Backgroundimage>
-            <div className="overlay-text">
+            <div className="overlay-text" id="0" ref={imagesec}>
                 <h2>Transform Your Data into Insights.</h2>
       <p className="mt-2">
       Empowering your business with tailored analytics solutions to drive smarter decisions and sustainable success.
@@ -24,14 +37,25 @@ const Tagzero=()=>{
       <button type="button" className="btn buttoncolor mt-2" 
       >Start Your Journey</button>
 
+   
     </div>
-
         
+<div ref={aboutUs} id="1">
+        <AboutUs ref={aboutUs} />
+        </div>
+        <div ref={services} id="2">
+        <Service />
+        </div>
+        
+        <div ref={howWeWork} id="3">
 
-        <AboutUs/>
-        <Service/>
-        <HowWeWork/>
-        <Casestudies/>
+        <HowWeWork />
+
+        </div>
+        <div ref={caseStudy} id="4">
+        <Casestudies />
+
+        </div>
         <Footer/>
         </>
     )
--- src/Containers/Navigation/TagzeroNav.js
+++ src/Containers/Navigation/TagzeroNav.js
@@ -1,26 +1,100 @@
-import {Logo} from "../../constants/svglocations"
-const TagzeroNav =()=>{
-    return(
-<nav class="navbar navbar-expand-lg navbgclr">
-  <div class="container-fluid d-flex justify-content-around ">
-    
-    <a class="navbar-brand" href="#"><Logo></Logo></a>
-    {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+import { Logo } from "../../constants/svglocations";
+import React, { useEffect, useState } from "react";
+const TagzeroNav = (props) => {
+  const [active, setactive] = useState();
+
+  const navclass = `nav-link   navfont navtextclr`;
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log(entry.target.id)
+          if(active==entry.target.id)
+            return
+          setactive(entry.target.id);
+        }
+        
+      });
+    }, { threshold: 0.5 }); // Trigger when 50% of the section is visible
+
+    const sections = [props.aboutUs.current, props.services.current, props.howWeWork.current,props.caseStudy.current,props.imagesec.current];
+    sections.forEach(section => observer.observe(section));
+
+    return () => {
+      sections.forEach(section => observer.unobserve(section));
+    };
+  }, []);
+
+
+  return (
+    <nav class="navbar navbar-expand-lg navbgclr sticky-sm-top">
+      <div class="container-fluid d-flex justify-content-around pt-2">
+        <a class="navbar-brand" href="#">
+          <Logo></Logo>
+        </a>
+        {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button> */}
-    
-      <div class="navbar-nav navbgclr">
-        <a class="nav-link   navfont navtextclr" aria-current="page" href="#">About Us</a>
-        <a class="nav-link  navfont navtextclr" href="#">Services</a>
-        <a class="nav-link  navfont navtextclr" href="#">Case Study</a>
+
+        <div class="navbar-nav navbgclr">
+          <a
+          
+            class={navclass}
+            style={{
+              color: active == "1" ? "#03BF71" : null,
+            }}
+            onClick={() => {
+              setactive("1");
+              props.scrollToSection(props.aboutUs);
+            }}
+          >
+            About Us
+          </a>
+          <a
+            class={navclass}
+            style={{
+              color: active == 2 ? "#03BF71" : null,
+            }}
+            onClick={() => {
+              setactive(2);
+
+              props.scrollToSection(props.services);
+            }}
+          >
+            Services
+          </a>
+          <a
+            class={navclass}
+            style={{
+              color: active == 3 ? "#03BF71" : null,
+            }}
+            onClick={() => {
+              setactive(3);
+
+              props.scrollToSection(props.howWeWork);
+            }}
+          >
+            How We Work
+          </a>
+          <a
+            class={navclass}
+            style={{
+              color: active == 4 ? "#03BF71" : null,
+            }}
+            onClick={() => {
+              setactive(4);
+
+              props.scrollToSection(props.caseStudy);
+            }}
+          >
+            Case Study
+          </a>
+        </div>
+        <button type="button" class="btn buttoncolor">
+          Contact Us
+        </button>
       </div>
-    <button type="button" class="btn buttoncolor">Contact Us</button>
-
-  </div>
-</nav>
-
-
-
-    )
-}
+    </nav>
+  );
+};
 export default TagzeroNav;
